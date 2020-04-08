8b5e1ab0fb4fa92fa2e91997bf7dc9200e0afda7
Hedrick Godson <hedrickgodson23@gmail.com>
2020-04-08 05:32:38 +0300

    Update phoneAndEmail.py
    Final Commit

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -36,4 +36,10 @@ for groups in phoneRegex.findall(text):
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
 
-
+# Copy results to the clipboard.
+if len(matches) > 0:
+        pyperclip.copy('\n'.join(matches))
+        print('Copied to clipboard:')
+        print('\n'.join(matches))
+else:
+        print('No phone numbers or email addresses found.')
