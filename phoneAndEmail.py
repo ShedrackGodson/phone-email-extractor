5ea066457056eb71792da23e94b2a4c9c7a15b40
ShedrackGodson <hedrickgodson23@gmail.com>
2020-04-08 05:23:37 +0300

    Phone Regex| Email Regex | Pyperclip

---
+++ phoneAndEmail.py
@@ -0,0 +1,39 @@
+import pyperclip,re
+
+
+# Create a Regex for Phone Numbers
+
+phoneRegex = re.compile(r'''(
+        ([200~(\d{3}|\(\d{3}\))?
+        (\s|-|\.)?
+        (\d{3})
+        (\s|-|\.)
+        (\d{4})
+        (\s*(ext|x|ext.)\s*(\d{2,5}))?
+        )''',re.VERBOSE)
+
+# Create a Regex for Email Addresses
+
+emailRegex = re.compile(r'''(
+        [a-zA-Z0-9._%+-]+
+        @
+        [a-zA-Z0-9.-]+
+        (\.[a-zA-Z]{2,4})
+        )''',re.VERBOSE)
+
+
+# Find All Matches in the Clipboard Text
+
+text = str(pyperclip.paster())
+matches = []
+
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1],groups[3],groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x'+ groups[8]
+    matches.append(phoneNum)
+
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+
